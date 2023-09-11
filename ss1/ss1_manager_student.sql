d6a7759b9e886fc6cef8fce41172f52cc6609946
anhtoan1210 <lequangtoan12102000@gmail.com>
2023-09-11 12:31:32 +0700

    bai tap

---
+++ ss1/ss1_manager_student.sql
@@ -0,0 +1,24 @@
+create database manager_student;
+use manager_student;
+
+create table classes(
+id int auto_increment primary key,
+name varchar(45)
+);
+
+create table teachers(
+id int auto_increment primary key,
+name varchar(45),
+age int,
+country varchar(45)
+);
+
+insert into classes(name)
+values ('Toản'),
+('Lâm');
+
+insert into teachers(name,age,country)
+values('Lâm',21,'Việt Nam'),
+('Sang',26,'Lào');
+
+SELECT name FROM mateachersteachersclassesclassesnager_student.teachers;
