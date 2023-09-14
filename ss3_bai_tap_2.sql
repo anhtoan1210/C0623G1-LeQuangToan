4e4e50d96619fa7ae177c683bc77069f095c56c5
anhtoan1210 <lequangtoan12102000@gmail.com>
2023-09-14 07:18:29 +0700

    bai tap

--- ss3_bai_tap_2.sql
+++ ss3_bai_tap_2.sql
@@ -30,4 +30,44 @@ foreign key (pID) references products(pID),
 odQTY int
 );
 
+insert into customer (cName,cAge)
+values('Minh Quân',10),
+('Ngọc Oanh',20),
+('Hong Ha',50);
+
+insert into orders(cID,cName,oTotalPrice)
+values(1,'3/21/2006',null),
+(2,'3/23/2006',null),
+(1,'3/16/2006',null);
+
+insert into products(pName,pPrice)
+values ('Máy giặc',3),
+('Tủ lạnh',5),
+('Điều hoà',7),
+('Quạt',1),
+('Bếp điện',2);
+
+insert into  OrderDetails(oID,pID,odQTY)
+values(1,1,3),
+(1,3,7),
+(1,4,2),
+(2,1,1),
+(3,1,8),
+(2,5,4),
+(2,3,3);
+
+select cID,cName,oTotalPrice
+from orders;
+
+select orders.oID,customer.cName
+from orders 
+join customer on orders.cID=customer.cID
+where orders.oID is null;
+
+select orders.oID,customer.cName
+from orders 
+join customer on orders.cID=customer.cID;
+
+
+
 
